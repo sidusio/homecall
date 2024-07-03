b831ed1a2bb780b6168ccc46e27ea26302c63162
Felicia <felicia@wallin.design>
2024-07-03 15:45:58 +0200

    feat: sentry

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,9 @@ import './assets/styles/main.scss'
 
 import { createAuth0 } from '@auth0/auth0-vue';
 import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import * as Sentry from "@sentry/vue";
 import { createPinia } from 'pinia'
 
 /* import the fontawesome core */
@@ -22,11 +25,23 @@ library.add(faGear)
 library.add(faPen)
 library.add(faTrash)
 
-
-import App from './App.vue'
-import router from './router'
-
 const app = createApp(App)
+
+Sentry.init({
+    app,
+    dsn: "https://9639d3406a54364151d90077a1a2020b@o4507538136170496.ingest.de.sentry.io/4507538144755792",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 app.use(
     createAuth0({
