afa7fe25c72660dd7b60631d3bc90540ea8064d4
Felicia <felicia@wallin.design>
2024-07-30 23:18:19 +0200

    fix: buildable after async/await error

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -22,67 +22,69 @@ library.add([faGear, faPlus, faChevronRight, faChevronUp, faChevronDown, faPen, 
 
 const app = createApp(App)
 
-const config = await getConfig();
-
-if(!allOptionsDefined(config)) {
-  console.log({config})
-  throw new Error('Not all options are defined in the config');
-}
 declare global {
   interface Window {
     config: Config;
   }
 }
-window.config = config;
 
-Sentry.init({
-    app,
-    dsn: config.sentry.dsn,
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration(),
-      Sentry.feedbackIntegration({
-        // Additional SDK configuration goes in here, for example:
-        colorScheme: "system",
-        showBranding: false,
-        triggerLabel: "Ge oss feedback",
-        formTitle: "Ge oss feedback",
-        nameLabel: "Namn",
-        namePlaceholder: "Skriv ditt namn här...",
-        emailLabel: "E-post",
-        emailPlaceholder: "Skriv din e-post här...",
-        messageLabel: "Meddelande",
-        messagePlaceholder: "Skriv ditt meddelande här...",
-        isRequiredLabel: "*",
-        addScreenshotButtonLabel: "Lägg till skärmbild",
-        removeScreenshotButtonLabel: "Ta bort skärmbild",
-        submitButtonLabel: "Skicka feedback",
-        cancelButtonLabel: "Avbryt",
-        successMessageText: "Tack för din feedback!",
-      }),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+getConfig().then((config) => {
+  if(!allOptionsDefined(config)) {
+    console.log({config})
+    throw new Error('Not all options are defined in the config');
+  }
 
-app.use(
-  createAuth0({
-    domain: config.auth0.domain,
-    clientId: config.auth0.clientId,
-    authorizationParams: {
-      redirect_uri: config.auth0.redirectUri,
-      audience: config.auth0.audience,
-    }
-  })
-);
-app.use(createPinia())
-app.use(router)
+  window.config = config;
 
-app.component('font-awesome-icon', FontAwesomeIcon)
+  Sentry.init({
+      app,
+      dsn: config.sentry.dsn,
+      integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+        Sentry.feedbackIntegration({
+          // Additional SDK configuration goes in here, for example:
+          colorScheme: "system",
+          showBranding: false,
+          triggerLabel: "Ge oss feedback",
+          formTitle: "Ge oss feedback",
+          nameLabel: "Namn",
+          namePlaceholder: "Skriv ditt namn här...",
+          emailLabel: "E-post",
+          emailPlaceholder: "Skriv din e-post här...",
+          messageLabel: "Meddelande",
+          messagePlaceholder: "Skriv ditt meddelande här...",
+          isRequiredLabel: "*",
+          addScreenshotButtonLabel: "Lägg till skärmbild",
+          removeScreenshotButtonLabel: "Ta bort skärmbild",
+          submitButtonLabel: "Skicka feedback",
+          cancelButtonLabel: "Avbryt",
+          successMessageText: "Tack för din feedback!",
+        }),
+      ],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
 
-app.mount('#app')
+  app.use(
+    createAuth0({
+      domain: config.auth0.domain,
+      clientId: config.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: config.auth0.redirectUri,
+        audience: config.auth0.audience,
+      }
+    })
+  );
+  app.use(createPinia())
+  app.use(router)
+
+  app.component('font-awesome-icon', FontAwesomeIcon)
+
+  app.mount('#app')
+})
