ded6b0f15e3b878b7e3a0c5892f1a092136d8fd1
Felicia <felicia@wallin.design>
2024-08-07 21:59:58 +0200

    fix: make sentry only work on prod

--- app/App.tsx
+++ app/App.tsx
@@ -10,12 +10,14 @@ import Call from "./views/Call";
 import * as Sentry from '@sentry/react-native';
 import {StatusBar} from 'react-native';
 
-Sentry.init({
-  dsn: 'https://9639d3406a54364151d90077a1a2020b@o4507538136170496.ingest.de.sentry.io/4507538144755792',
+if(!__DEV__) {
+  Sentry.init({
+    dsn: 'https://9639d3406a54364151d90077a1a2020b@o4507538136170496.ingest.de.sentry.io/4507538144755792',
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // enableSpotlight: __DEV__,
-});
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // enableSpotlight: __DEV__,
+  });
+}
 
 // Hopefully make TextEncoder available
 applyGlobalPolyfills()
@@ -85,4 +87,10 @@ function App() {
   );
 }
 
-export default Sentry.wrap(App);
+let app: React.ComponentType = App;
+
+if(!__DEV__) {
+  app = Sentry.wrap(App);
+}
+
+export default app;
